c5ee57539a0dbbd9b342ced4006fe98737dd6221
Abe-022 <111305120+Abe-022@users.noreply.github.com>
2025-10-31 18:02:42 -0600

    Initial commit

---
+++ Tarjetas/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Inicio Plantillas</title>
+  <meta name="robots" content="noindex, follow">
+</head>
+  <body>
+    
+    <div class="body-index"  style="position:absolute">
+        <div class="Titulo">
+          <h1></h1>
+        </div>
+    </div>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Inicio Plantillas</title>
+  <link rel="stylesheet" href="common/css/index.css">
+  <meta name="robots" content="noindex, follow">
+</head>
+  <body>
+    
+    <div class="body-index" >
+        <div class="loginScreen">
+          <h1>Plantillas</h1>
+        </div>
+        <div class="flexArea">
+          <a class="flexBloque" href="Tarjetas/index.php">menus</a>
+        </div>
+        <div class="gridArea">
+          <div class="gridBloque">Grid Bloque 1</div>
+          <div class="gridBloque">Grid Bloque 2</div>
+          <div class="gridBloque">Grid Bloque 3</div>
+          <div class="gridBloque">Grid Bloque 4</div>
+          <div class="gridBloque">Grid Bloque 5</div>
+          <div class="gridBloque">
+            <div class="grid1">grid 1</div>
+            <div class="grid1">grid 2</div>
+            <div class="grid1">grid 3</div>
+          </div>
+        </div>
+        <div class="flexArea">
+          <div class="flexBloque">Flex Bloque 1</div>
+          <div class="flexBloque">Flex Bloque 2</div>
+          <div class="flexBloque">Flex Bloque 3</div>
+          <div class="flexBloque">Flex Bloque 4</div>
+          <div class="flexBloque">Flex Bloque 5</div>
+          <div class="flexBloque">
+            <div class="flex1">Flex 1</div>
+            <div class="flex1">Flex 2</div>
+            <div class="flex1">Flex 3</div>
+          </div>
+        </div>
+
+        <div class="gridArea">
+          <div class="flexBloque">Flex Bloque 1</div>
+          <div class="flexBloque">Flex Bloque 2</div>
+          <div class="flexBloque">Flex Bloque 3</div>
+          <div class="flexBloque">Flex Bloque 4</div>
+          <div class="flexBloque">Flex Bloque 5</div>
+          <div class="flexBloque">
+            <div class="grid1">Flex 1</div>
+            <div class="grid1">Flex 2</div>
+            <div class="grid1">Flex 3</div>
+          </div>
+        </div>
+
+        <div class="flexArea">
+          <div class="gridBloque">Flex Bloque 1</div>
+          <div class="gridBloque">Flex Bloque 2</div>
+          <div class="gridBloque">Flex Bloque 3</div>
+          <div class="gridBloque">Flex Bloque 4</div>
+          <div class="gridBloque">Flex Bloque 5</div>
+          <div class="gridBloque">
+            <div class="flex1">Flex 1</div>
+            <div class="flex1">Flex 2</div>
+            <div class="flex1">Flex 3</div>
+          </div>
+        </div>
+
+        <div class="flexArea">
+          <div class="flexBloque">Flex Bloque 1</div>
+          <div class="flexBloque">Flex Bloque 2</div>
+          <div class="flexBloque">Flex Bloque 3</div>
+          <div class="flexBloque">Flex Bloque 4</div>
+          <div class="flexBloque">Flex Bloque 5</div>
+          <div class="flexBloque">
+            <div class="grid1">Flex 1</div>
+            <div class="grid1">Flex 2</div>
+            <div class="grid1">Flex 3</div>
+          </div>
+        </div>
+
+        <div class="gridArea">
+          <div class="gridBloque">Flex Bloque 1</div>
+          <div class="gridBloque">Flex Bloque 2</div>
+          <div class="gridBloque">Flex Bloque 3</div>
+          <div class="gridBloque">Flex Bloque 4</div>
+          <div class="gridBloque">Flex Bloque 5</div>
+          <div class="gridBloque">
+            <div class="flex1">Flex 1</div>
+            <div class="flex1">Flex 2</div>
+            <div class="flex1">Flex 3</div>
+          </div>
+        </div>
+    </div>
+  </body>
+</html>

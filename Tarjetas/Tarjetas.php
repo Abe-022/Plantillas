42a11d31c73f1f971ee2443645da3ca77b17245a
Abe-022 <111305120+Abe-022@users.noreply.github.com>
2025-11-04 00:57:36 -0600

    actualización

--- Tarjetas/Tarjetas.php
+++ Tarjetas/Tarjetas.php
@@ -1,12 +1,14 @@
 <?php
     $imagenDelatera="";
-    $imagenTrasera="/common/img/credencial/tarjetaTrasera2023-1.png";
 ?> 
 <!DOCTYPE html>
 <html>
 <head>
   <title>Inicio Plantillas</title>
   <link rel="stylesheet" href="common/css/index.css">
+  
+  <link rel="stylesheet" href="../common/includes/frameworks/bootstrap/css/bootstrap.min.css" >
+  <link rel="stylesheet" href="../common/includes/frameworks/bootstrap/bootstrap-icons-1.10.3/bootstrap-icons.css">
   <meta name="robots" content="noindex, follow">
 </head>
   <body>
@@ -21,15 +23,24 @@
           <div class="tarjeta">
             <div class="flexArea TarjetaCredito">
               <div class="delantera" style="background:url(common/img/nu.png);">
-                <div class="fechaExpiracion">12/24</div>
+                <div id="nombrePersona" class="nombrePersona">nombre apellido</div>
               </div>
               <div class="trasera" >
-                <div class="numeroTarjeta">1234 5678 9012 3456</div>
+                <div id="numeroTarjeta" class="numeroTarjeta">1234 5678 9012 3456</div>
               </div>
             </div>
           </div>
           
-          <div class="flexBloque" href="Tarjetas/index.php">Tarjetas</div>
+          <div class="FormularioTarjeta">
+            <div class="form-floating is-invalid">
+              <input id="txtNombre" name="txtNombre" type="text" class="form-control" maxlength="60" autocomplete="off">
+              <label id="lbNombre" for="txtNombre">nombre completo:</label>
+            </div>
+            <div class="form-floating is-invalid">
+              <input id="txtNoTarjeta" name="txtNoTarjeta" type="text" class="form-control" maxlength="60" autocomplete="off">
+              <label id="lbNoTarjeta" for="txtNoTarjeta">numero tarjeta:</label>
+            </div>
+          </div>
         </div>
     </div>
 
--- Tarjetas/common/js/tarjetas.js
+++ Tarjetas/common/js/tarjetas.js
@@ -3,3 +3,30 @@ $(".tarjeta ").on("click", function(){
     tarjeta.toggleClass("active");
 });
 
+function rotarTarjeta(){
+    tarjeta.toggleClass("active");
+}
+let card = 1;
+
+$("#txtNombre").keyup(function(){
+    if(card!=1){
+        rotarTarjeta();
+    }
+    mostrarDatos();
+    card = 1;
+});
+$("#txtNoTarjeta").keyup(function(){
+    mostrarDatos();
+    if(card!=2){
+        rotarTarjeta();
+    }
+    card = 2;
+});
+
+function mostrarDatos(){
+    let nombre = document.getElementById("txtNombre").value.toUpperCase();
+    let noTarjeta = document.getElementById("txtNoTarjeta").value.toUpperCase();
+    //console.log(nombre);
+    jQuery("#nombrePersona").html(nombre);
+    jQuery("#numeroTarjeta").html(noTarjeta);
+}
